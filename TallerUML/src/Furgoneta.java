9deb46573a042507465675c2297813ec1ca8baea
DylanEscala <descala@espol.edu.ec>
2020-11-19 16:52:45 -0500

    Furgoneta

---
+++ TallerUML/src/Furgoneta.java
@@ -0,0 +1,18 @@
+
+public class Furgoneta {
+private int capCarga;
+private double pesoSinCarga;
+public int getCapCarga() {
+	return capCarga;
+}
+public void setCapCarga(int capCarga) {
+	this.capCarga = capCarga;
+}
+public double getPesoSinCarga() {
+	return pesoSinCarga;
+}
+public void setPesoSinCarga(double pesoSinCarga) {
+	this.pesoSinCarga = pesoSinCarga;
+}
+
+}
